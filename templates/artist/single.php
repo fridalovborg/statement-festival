da22cf4d4e995fba9795954b5c0b1b3b969bb8e8
Frida Lövborg <>
2018-01-24 19:48:24 +0100

    Changes size of feature image and added a text from acf

--- templates/artist/single.php
+++ templates/artist/single.php
@@ -8,15 +8,17 @@
 <div class="container">
 	<div class="row">
 		<div class="col-12 col-sm-12 col-md-1"></div>
-		<div class="col-12 col-sm-12 col-md-6">
+		<div class="col-12 col-sm-12 col-md-5">
     		<h2><?php the_title(); ?></h2>
 			<?php the_content(); ?>
 		</div>
-		<div class="col-12 col-sm-12 col-md-4">
+		<div class="col-12 col-sm-12 col-md-5">
 			<!-- FEATURE IMAGE -->
-			<div class="feature-img">
-				<?php the_post_thumbnail('small'); ?>
-			</div>
+			<div class="feature-img" style="background-image: url('<?php echo get_the_post_thumbnail_url( null, 'large' ); ?>"></div>
+			
+			<?php if( get_field('photograph') ): ?>
+				<p class="italic">Photo: <?php the_field('photograph'); ?></p>			
+			<?php endif; ?>
 		</div>
 	</div>
 	<div class="row">
@@ -33,6 +35,12 @@
 				<?php if( get_field('sound_cloud') ): ?>
 				<a class="icons" href="<?php the_field('sound_cloud'); ?>" target="_blank">
 					<img src="<?php echo get_template_directory_uri(); ?>/dist/images/sound_cloud.svg">
+				</a>
+				<?php endif; ?>
+
+				<?php if( get_field('facebook') ): ?>
+				<a class="icons" href="<?php the_field('facebook'); ?>" target="_blank">
+					<img src="<?php echo get_template_directory_uri(); ?>/dist/images/facebook.svg">
 				</a>
 				<?php endif; ?>
 			</div>
