3f2dec008b873172c7dc4d229f077a02e2ba3504
Frida Lövborg <>
2018-01-25 20:04:25 +0100

    artist archive images changes

--- templates/artist/archive.php
+++ templates/artist/archive.php
@@ -8,14 +8,13 @@
 $query = new WP_Query( array( 'post_type' => 'artist' ) );
 $query->set( 'posts_per_page', '-1' );
 ?>
-<?php $artistAll = get_the_post_thumbnail_url(); ?>
 <div class="container">
 	<div class="row">
 		<?php while (have_posts()) : the_post(); ?>
-			<div class="col-6 col-sm-6 col-md-4 col-lg-3">
+			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 				<a class="artist-container" href="<?php the_permalink(); ?>">
 					<h2 class="artist-title"><?php the_title(); ?></h2>
-					<div class="feature-img-artist" style="background-image: url('<?php echo $artistAll; ?>')"></div>
+					<?php the_post_thumbnail( 'x-small', array('class' => 'square-img') ); ?>
 				</a>
 			</div>
 		<?php endwhile; ?>
